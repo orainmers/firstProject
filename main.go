aafcfad7de780ce493537c3f5fcc685f87143c68
orainmers <arnautvladislav5@gmail.com>
2024-11-25 23:50:35 +0300

    add GET and POST

--- main.go
+++ main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gorilla/mux"
+	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+type response struct {
+	Head string `json:"head"`
+	Body string `json:"body"`
+}
+type requestBody struct {
+	Message string `json:"message"`
 }
 
+var task string
+
+func PostHandler(ctx echo.Context) error {
+	var request requestBody
+	if err := ctx.Bind(&request); err != nil {
+		return ctx.JSON(http.StatusBadRequest, response{
+			Head: "Error",
+			Body: "Message not added",
+		})
+	}
+	task = request.Message
+	return ctx.JSON(http.StatusOK, response{
+		Head: "Success",
+		Body: "Message was successfully added",
+	})
+}
+
+func HelloHandler(ctx echo.Context) error {
+	return ctx.JSON(http.StatusOK, "Hello, "+task)
+}
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
-	http.ListenAndServe(":8080", router)
+	ctx := echo.New()
+	ctx.GET("/api/hello", HelloHandler)
+	ctx.POST("/api/hello", PostHandler)
+	ctx.Start(":8080")
 }
