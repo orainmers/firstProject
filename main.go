5ff3bceaca1473b74497cc5d74c864471b0da099
orainmers <arnautvladislav5@gmail.com>
2024-11-29 15:16:19 +0300

    patch Get and Post

--- main.go
+++ main.go
@@ -2,40 +2,58 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"log"
 	"net/http"
 )
 
-type response struct {
-	Head string `json:"head"`
-	Body string `json:"body"`
-}
-type requestBody struct {
-	Message string `json:"message"`
+type Response struct {
+	Status string `json:"status"`
+	Detail string `json:"detail"`
 }
 
-var task string
-
-func PostHandler(ctx echo.Context) error {
-	var request requestBody
-	if err := ctx.Bind(&request); err != nil {
-		return ctx.JSON(http.StatusBadRequest, response{
-			Head: "Error",
-			Body: "Message not added",
+func GetHandler(c echo.Context) error {
+	var messages []Message
+	if err := DB.Find(&messages).Error; err != nil {
+		return c.JSON(http.StatusBadRequest, Response{
+			Status: "Error",
+			Detail: "Could not find the messages",
 		})
 	}
-	task = request.Message
-	return ctx.JSON(http.StatusOK, response{
-		Head: "Success",
-		Body: "Message was successfully added",
+	return c.JSON(http.StatusOK, &messages)
+}
+func PostHandler(c echo.Context) error {
+	var task Message
+	if err := c.Bind(&task); err != nil {
+		return c.JSON(http.StatusBadRequest, Response{
+			Status: "Error",
+			Detail: "Message not added",
+		})
+	}
+	if err := DB.Create(&task).Error; err != nil {
+		return c.JSON(http.StatusBadRequest, Response{
+			Status: "Error",
+			Detail: "Could not create the message",
+		})
+	}
+	return c.JSON(http.StatusOK, Response{
+		Status: "Success",
+		Detail: "Message was successfully added",
 	})
 }
 
-func HelloHandler(ctx echo.Context) error {
-	return ctx.JSON(http.StatusOK, "Hello, "+task)
-}
 func main() {
-	ctx := echo.New()
-	ctx.GET("/api/hello", HelloHandler)
-	ctx.POST("/api/hello", PostHandler)
-	ctx.Start(":8080")
+	InitDB()
+
+	if err := DB.AutoMigrate(&Message{}); err != nil {
+		log.Fatalf("Не удалось мигрировать данные: %v", err)
+	}
+
+	c := echo.New()
+
+	c.GET("/api/hello", GetHandler)
+	c.POST("/api/hello", PostHandler)
+
+	if err := c.Start(":8080"); err != nil {
+		log.Fatalf("Не удалось запустить сервер: %v", err)
+	}
 }
